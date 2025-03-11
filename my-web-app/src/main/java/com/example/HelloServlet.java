9c7a4198541cfe9d68d30c6af0eba277c834c824
Aayush <ayush85v@gmail.com>
2025-03-11 11:25:07 +0530

    WAR

--- my-web-app/src/main/java/com/example/HelloServlet.java
+++ my-web-app/src/main/java/com/example/HelloServlet.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.javapoint;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- my-web-app/src/test/java/com/example/CalculatorServletTest.java
+++ my-web-app/src/test/java/com/example/CalculatorServletTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.javapoint;
 
 import org.junit.Test;
 import org.mockito.Mockito;
